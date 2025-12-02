d67e2d78eef1d82b96a06ecd1c8c4c15aa7736fa
ZainabFullStackLearner <farooqamal124@gmail.com>
2025-12-02 17:48:32 +0500

    first commit

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -3,11 +3,11 @@ import Stripe from 'stripe';
 import { CartItem } from '@/app/context/Context';
 import { auth } from '@clerk/nextjs/server';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-11-17.clover",
-});
-
 export async function POST(req: NextRequest) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2025-11-17.clover",
+  });
+
   const { userId } = await auth();
 
   if (!userId) {
